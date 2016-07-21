eece6e9eeeddbac257125b331fc2e7b1b9690226
clair13 <theclairvoyant1313@yahoo.gr>
2016-07-21 14:48:03 +0200

    updating gemfile

--- test/controllers/pics_controller_test.rb
+++ test/controllers/pics_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class PicsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::Helpers                          
+  include Warden::Test::Helpers                        
+  Warden.test_mode!                                    
+
+  #def teardown                                         
+  #  Warden.test_reset!                                 
+  #end                           
+
+  #setup do 
+  #	@pic = pics(:one)
+  #end
+  	
+  test "should get index" do
+  	get :index
+  	assert_response :success
+    assert_not_nil assigns(:pics)
+  end
+
+  test "should get new" do
+  	get :new
+  	assert_response :success
+  end
+
+  test "should show pic" do
+    get :show, id: @pic
+    assert_response :success
+  end
+
+  test "should update pic" do
+    patch :update, id: @pic, pic: { description: @pic.description}
+    assert_redirected_to pics_path(assigns(:pic)) 
+  end
+
+  test "should destroy pic" do
+    assert_difference('Pic.count', -1)do
+      delete :destroy, id: @pic
+    end
+
+    assert_redirected_to pics_path
+  end
+
 end
